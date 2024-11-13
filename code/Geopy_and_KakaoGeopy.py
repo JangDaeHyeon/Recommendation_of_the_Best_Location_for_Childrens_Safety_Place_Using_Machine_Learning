37726fd9e7030a4c8e318887aa25c840e3185355
Jugahy <82772374+Jugahy@users.noreply.github.com>
2024-11-13 13:25:05 +0900

    Add files via upload

---
+++ code/Geopy_and_KakaoGeopy.py
@@ -0,0 +1,51 @@
+import requests, json, pandas as pd
+from geopy.geocoders import Nominatim
+
+geo_local = Nominatim(user_agent="South Korea")
+
+pd.set_option('display.max_rows', None)
+
+
+def get_location(address):
+    url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + address
+    headers = {"Authorization": "KakaoAK b0fb3729d2fb8c9d30c1d622f364130d"}
+    api_json = json.loads(str(requests.get(url, headers=headers).text))
+    address = api_json['documents'][0]['address']
+    crd = [str(address['y']), str(address['x'])]
+    address_name = address['address_name']
+
+    return crd
+
+
+FileName = "/Data/공공데이터포털/통합/고창군_통합.csv"
+data = pd.read_csv(FileName, encoding="euc-kr")
+addr = data["주소"].copy()
+# addr = ["전라북도 군산시 백토로 202","전라북도 전주시 완산구 팔달로 77","전라북도 전주시 덕진구 송천로 35-15","전라북도 익산시 인북로 112"]
+
+latitudes = []
+longitudes = []
+er = []
+
+for i in range(len(addr)):
+    try:
+        crd = get_location(addr[i])
+        latitudes.append(crd[0])
+        longitudes.append(crd[1])
+        print(i, addr[i], crd)
+    except IndexError:
+        try:
+            location = geo_local.geocode(addr[i])
+            latitudes.append(str(location.latitude))
+            longitudes.append(str(location.longitude))
+            print(i, addr[i], str(location.latitude))
+        except AttributeError:
+            latitudes.append(0)
+            longitudes.append(0)
+            print(i + 2, addr[i])
+            er.append(i + 2)
+            pass
+
+data["위도"] = latitudes
+data["경도"] = longitudes
+
+data.to_csv("C:/Users/jugah/PycharmProjects/2022-DataAnalysis/Data/공공데이터포털/통합/고창군_통합.csv", encoding="euc-kr")
---
+++ code/k_means_algorithm.py
@@ -0,0 +1,157 @@
+def korean():
+    from matplotlib import font_manager, rc
+
+    font_path = "C:/Windows/Fonts/NGULIM.TTF"
+    font = font_manager.FontProperties(fname=font_path).get_name()
+    rc('font', family=font)
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+
+data = []
+
+df = pd.read_csv("C:/Users/jugah/PycharmProjects/2022-DataAnalysis/Data/공공데이터포털/전북_통합.csv", encoding="euc-kr")
+
+for i in range(len(df)):
+    data.append([df.loc[i]["위도"], df.loc[i]["경도"]])
+# data = [[1,1], [1,2], [1,3], [1,4], [1,5], [2,1], [2,2], [2,3], [2,4], [2,5], [3,1], [3,2], [3,3], [3,4], [3,5], [4,1], [4,2], [4,3], [4,4], [4,5], [5,1], [5,2], [5,3], [5,4], [5,5],[20,20], [20,21], [20,22]]
+
+k = list(random.sample(range(0,len(df)), k=10))
+
+
+# k = [0,2,4,5,25,27,26]
+# 데이터 형식 : [[위도1,경도1], [위도2,경도2], ...[위도n,경도n]]
+# k 형식 : data의 값 중 원하는 좌표의 인덱스 [0,8]
+def put(data, k):
+    # 새로운 평균값을 저장해 놓는 리스트 인덱스 0,1이 data
+    mid_list = []
+    for i in range(len(k) * 2):
+        mid_list.append([0, 0])
+    print(mid_list)
+
+
+# data의 좌표를 이용할 때 군집 나누는 함수
+def cluster():
+    import math
+
+    global remember_group
+
+    distance = []
+    group = []
+    remember_group = []
+
+    for i in range(len(data)):
+        distance.append([])
+
+        for j in k:
+            distance[i].append(math.sqrt((data[i][0] - data[j][0]) ** 2 + (data[i][1] - data[j][1]) ** 2))
+
+    for i in range(len(data)):
+        group.append(k[distance[i].index(min(distance[i]))])
+
+    remember_group = group
+    # print(distance)
+
+    return remember_group
+
+
+# 새로운 평균값 만드는 함수
+def mid():
+    global new_mid
+    global remember_group
+    try:
+        remember_group = group
+    except NameError:
+        pass
+    all_mid = []
+    new_mid = []
+
+    all_mid.append([])
+    all_mid.append([])
+
+    for i in range(len(k)):
+        new_mid.append([])
+
+        for j in [l for l, x in enumerate(remember_group) if x == k[i]]:
+            all_mid[0].append(data[j][0])
+            all_mid[1].append(data[j][1])
+        try:
+            new_mid[i].append(sum(all_mid[0]) / remember_group.count(k[i]))
+            new_mid[i].append(sum(all_mid[1]) / remember_group.count(k[i]))
+        except ZeroDivisionError:
+            pass
+
+        all_mid[0] = []
+        all_mid[1] = []
+    return new_mid
+
+
+# 새로운 평균값의 좌표를 이용할 때 군집 나누는 함수
+def k_cluster():
+    import math
+    global group
+    reremember_group = []
+    count = 0
+    while True:
+        count += 1
+        distance = []
+        group = []
+
+        for i in range(len(data)):
+            distance.append([])
+
+            for j in new_mid:
+                try:
+                    distance[i].append(math.sqrt((data[i][0] - j[0]) ** 2 + (data[i][1] - j[1]) ** 2))
+                except IndexError:
+                    pass
+        print(distance)
+        for i in range(len(data)):
+            group.append(k[distance[i].index(min(distance[i]))])
+
+        if remember_group == group:
+            print(count, "번째 그룹:", group)
+            print(count, "번째 평균갑:", new_mid)
+            print("count", count)
+
+            break
+        elif reremember_group == group:
+            print(count, "번째 그룹:", group)
+            print(count, "번째 평균갑:", new_mid)
+            print("count", count)
+            break
+        else:
+            print(count, "번째 그룹:", group)
+            print(count, "번째 평균값:", new_mid)
+            reremember_group = group
+            mid()
+
+
+    return group
+
+
+cluster()
+mid()
+for i in range(len(data)):
+    plt.scatter(data[i][1], data[i][0], color="black")
+
+for i in range(len(new_mid)):
+    try:
+        plt.scatter(new_mid[i][1], new_mid[i][0], color="red")
+    except IndexError:
+        pass
+plt.show()
+k_cluster()
+
+for i in range(len(data)):
+    plt.scatter(data[i][1], data[i][0], color="black")
+
+for i in range(len(new_mid)):
+    try:
+        plt.scatter(new_mid[i][1], new_mid[i][0], color="red")
+    except IndexError:
+        pass
+
+plt.show()
